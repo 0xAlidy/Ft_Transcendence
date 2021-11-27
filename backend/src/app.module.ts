08135e5d930272853c02010e936c129e7f92c676
Axel Lidy <alidy@student.42lyon.fr>
2021-11-27 09:58:18 +0100

    DB connect backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,13 @@ import { config } from './orm.config';
 import { WsModule } from './ws/ws.module';
 import { AppController } from './app.controller';
 import { HttpModule} from '@nestjs/axios';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [WsModule, ChatGateway, HttpModule],
+  imports: [WsModule, ChatGateway, HttpModule, TypeOrmModule.forRoot(config)],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
