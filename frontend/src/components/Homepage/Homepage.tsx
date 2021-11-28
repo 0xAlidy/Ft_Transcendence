bda7969c14c3838079318aa52954c5f0c8560db4
edep <eliott.dpw@gmail.com>
2021-11-28 21:46:09 +0100

    homepage rebuild

--- frontend/src/components/Homepage/Homepage.tsx
+++ frontend/src/components/Homepage/Homepage.tsx
@@ -1,16 +1,29 @@
 import Logo from './Logo'
 import React from 'react'
+import TwoDivs from './apiIframe'
 
-function Homepage() {
+export default class Homepage extends React.Component {
+    state = {
+        div1Shown: true,
+      }
+
+      handleButtonClick() {
+        this.setState({
+          div1Shown: false,
+        });
+        if (this.state.div1Shown)
+          (document.getElementById('item') as HTMLElement).style.marginTop = '5%';
+        else
+          (document.getElementById('item') as HTMLElement).style.marginTop = '25%';
+      }
+      render(){
     return (
-        <div className="logo">
-            <Logo/>
-            <a  className="link" href="https://api.intra.42.fr/oauth/authorize?client_id=d42d44ee8052b31b332b4eb135916c028f156dbb4d3c7e277030f3b2bc08d87c&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2F&response_type=code">\
-                <button className="loginButton">PLAY
-                </button>
-            </a>
+        <div className="logo"><Logo />
+        {
+          this.state.div1Shown ? (<button className="loginButton" onClick={() => this.handleButtonClick()}>PLAY</button>): (<iframe src="https://api.intra.42.fr/oauth/authorize?client_id=d42d44ee8052b31b332b4eb135916c028f156dbb4d3c7e277030f3b2bc08d87c&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2F&response_type=code" className="iframe" style={{zIndex:"4"}} width="300px" height="400px"></iframe>)
+        }
         </div>
-        )
-}
+   )
+};
 
-export default Homepage
+}
