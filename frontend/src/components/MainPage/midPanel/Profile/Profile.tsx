ccdad284801484f8a07c7784632116bd82db14d1
dewedw <dweew>
2021-12-08 11:35:47 +0100

    profile page 50%

--- frontend/src/components/MainPage/midPanel/Profile/Profile.tsx
+++ frontend/src/components/MainPage/midPanel/Profile/Profile.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import '../../../../styles/MainPage/midPanel/midPanel.css'
 import '../../../../styles/MainPage/midPanel/Profile/Profile.css'
-
+import DELETE from '../../../../assets/delete.png'
+import LEAVE from '../../../../assets/exit.png'
+import EditBox from './editBox';
 export default class Profile extends React.Component<{},{editMode:boolean}>{
 	constructor(props :any){
 		super(props)
 	}
-
 	render(){
 		return (
         <div className="midPanel" id="profile">
 			<img src="https://cdn.intra.42.fr/users/medium_default.png" className="profileImg"/>
-			<editBox value="saucisson" />
-			<div className="profilePseudo">edepauw</div>
-			<div className="profileLogout"></div>
-			<div className="profileDelete"></div>
+			<EditBox value="login" placeHolder="nickname"/>
+			<img src={LEAVE} alt="" width="50px"/>
+			<img src={DELETE} alt="" width="50px" />
 		</div>
     	)
 	}
--- frontend/src/components/MainPage/midPanel/Profile/editBox.tsx
+++ frontend/src/components/MainPage/midPanel/Profile/editBox.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
-
-export default class editBox extends React.Component<{value:string},{editMode:boolean}>{
+import '../../../../styles/MainPage/midPanel/Profile/EditBox.css'
+import EDIT from '../../../../assets/edit-button.png'
+import DONE from '../../../../assets/check.png'
+export default class EditBox extends React.Component<{value:string, placeHolder:string},{editMode:boolean}>{
 	constructor(props :any){
 		super(props)
-		this.state= {editMode: true}
+		this.state = {editMode: false}
+		this.click = this.click.bind(this);
+	}
+
+	click() {
+		if (this.state.editMode)
+			this.setState({editMode: false});
+		else
+			this.setState({editMode: true});
 	}
 
 	render(){
 		return (
         <div className="editBox">
 			{
-				this.state.editMode === true ? <div>{this.props.value}</div>:<input type="text"/>
+				this.state.editMode === true ? <input placeholder={this.props.placeHolder} className="ProfileInput"/>: <div className="ProfileInput">{this.props.value}</div>
+			}
+			{
+				this.state.editMode === false ? <img src={EDIT} alt="" className="editButton" onClick={this.click}/>: <img src={DONE} alt="" onClick={this.click} className="editButton"/>
 			}
 		</div>
     	)
