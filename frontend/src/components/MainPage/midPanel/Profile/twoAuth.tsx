7d35882eadc7a8cc1327dbf88ef88438413d47a5
Axel Lidy <alidy@student.42lyon.fr>
2022-01-20 16:29:59 +0100

    2fa

--- frontend/src/components/MainPage/midPanel/Profile/twoAuth.tsx
+++ frontend/src/components/MainPage/midPanel/Profile/twoAuth.tsx
@@ -3,63 +3,78 @@ import axios from 'axios'
 import '../../../../styles/MainPage/midPanel/midPanel.css'
 import '../../../../styles/MainPage/midPanel/Profile/twoAuth.css'
 
-export default class TwoAuth extends React.Component<{token:string},{qr:string | null, number:number | null, formError: boolean | null}>{
+export default class TwoAuth extends React.Component<{token:string},{secretEnabled:boolean, qr:string, number:number | null, verify:boolean | null}>{
 
 	constructor(props:any) {
 		super(props);
-		this.state = { 
-			qr: null,
+		this.state = {
+			secretEnabled: false,
+			qr: "",
 			number: null,
-			formError: null,
+			verify: null,
 		};
+		this.secretEnabled = this.secretEnabled.bind(this);
 		this.click = this.click.bind(this);
-		this.handleChange= this.handleChange.bind(this);
+		this.handleNumberChange= this.handleNumberChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	};
 
-	async click (){
-		await axios.post("HTTP://localhost:667/user/generateSecret", {token: this.props.token}).then(resp =>
-			this.setState({ qr: resp.data })
-		);
+	async secretEnabled(){
+		await axios.post("HTTP://localhost:667/user/secretEnabled", {token: this.props.token}).then(resp =>
+				this.setState({ secretEnabled: resp.data })
+			);
 	}
 
-	handleChange(event:any){
+	async click(){
+		if (!this.state.qr)
+		{
+			await axios.post("HTTP://localhost:667/user/generateSecret", {token: this.props.token}).then(resp =>
+				this.setState({ qr: resp.data })
+			);
+		}
+	}
+
+	handleNumberChange(event:any){
 		this.setState({ number: event.target.value});
 	}
 
 	async handleSubmit(event:any) {
-		event.preventDefault()
-		
-		await axios.post("HTTP://localhost:667/user/verifyNumber", {token: this.props.token, number: this.state.number})
+		event.preventDefault();
+		await axios.post("HTTP://localhost:667/user/verifyNumber", {token: this.props.token, number: this.state.number}).then(resp =>
+			this.setState({ verify:resp.data })
+		);
 	}
 
 	
 	render(){
-		let twoAuth;
-		let message;
-
-		/*if (this.state.formError == true)
-		else if (this.state.formError == false)*/
-		if (this.state.qr) {
-			twoAuth =  (
-				<div>
-					<h2>Veuillez scanner ce Qrcode sur votre application d'authentification: </h2>
-					<img src={this.state.qr} alt="QrCode" />
-					<form onSubmit={this.handleSubmit}>
-						<label>Saisir le code de votre application: </label>
-						<input type="number" name="code" id="number" onChange= {this.handleChange}></input>
-						<button>Verifier</button>
-						{message}
-					</form>
-				</div>
-    		);
-  		}
+		// Check secretEnabled
+		this.secretEnabled();
 		return (
-				<div id="twoAuth">
-					<p>Two-factor Authentication (2FA):</p>
-					<button onClick={this.click}>{} 2FA</button>
-					{twoAuth}
-				</div>
-			)
+			<div id="twoAuth">
+				<p>Two-factor Authentication (2FA):</p>
+				{
+					this.state.secretEnabled ?
+						<p>Deja activé</p>
+					:
+					<>
+						<button onClick={this.click}> 2FA </button>
+						{
+							this.state.qr && 
+							<div>
+								<h2>Veuillez scanner ce Qrcode sur votre application d'authentification: </h2>
+								<img src={this.state.qr} alt="QrCode" />
+								<form onSubmit={this.handleSubmit}>
+									<label>Saisir le code de votre application: </label>
+									<input type="number" name="code" id="number" onChange= {this.handleNumberChange}></input>
+									<button>Verifier</button>
+									{ this.state.verify === true ? <h2 style={{color:'green'}}>Bravo l'auth est active !</h2> : null }
+									{ this.state.verify === false ? <h2 style={{color:'red'}}>Le code est invalide !</h2> : null }
+								</form>
+							</div>
+						}
+					</>
+				}
+			</div>
+		)
 	};
 };
