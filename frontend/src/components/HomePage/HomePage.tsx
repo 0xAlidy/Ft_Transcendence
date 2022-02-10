99afa037a18ef9f9858bebf8af61aa308fb24b69
Axel Lidy <alidy@student.42lyon.fr>
2022-02-10 13:10:16 +0100

    css

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -5,7 +5,7 @@ import '../../styles/HomePage/HomePage.css'
 function HomePage(){
                           
   function handleButtonClick(){
-    var id = "f8d90302a7cbd7aad44c9c38c89d012b978c5a4555b85aa6a18f683fabcc3c47";
+    var id = "d42d44ee8052b31b332b4eb135916c028f156dbb4d3c7e277030f3b2bc08d87c";
     window.location.href = 'https://api.intra.42.fr/oauth/authorize?client_id=' + id + '&redirect_uri=http%3A%2F%2F'+ window.location.host.split(":").at(0) + '%3A667%2Fauth%2Fredirect&response_type=code';
   }
   return (
