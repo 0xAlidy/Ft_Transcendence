d6cd0017ff090c0dce7cb9961eb34a4430a502ae
tgrangeo <thomas.grangeon9@gmail.com>
2022-03-01 10:08:10 +0100

    patch_auth

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -3,22 +3,20 @@ import { UsersService } from 'src/user/users.service';
 
 @Injectable()
 export class AuthService {
-	constructor(private usersService: UsersService) {}
+    constructor(private usersService: UsersService) {}
 
-	async validateUser(token :string, login:string): Promise<any>
-	{
-		let user = await this.usersService.findOneByLogin(login);
-		if (user.status !== 1)
-		{
-			if (user && user.token !== token) {
-				user = await this.usersService.changetoken(login, token);
-			}
-			if (!user) {
-				user = await this.usersService.create(login, token);
-			}
-			const { ...result } = user;
-			return result;
-		}
-		return null;
-	}
+    async validateUser(token :string, login:string): Promise<any>
+    {
+        let user = await this.usersService.findOneByLogin(login);
+                if (user && user.token !== token) {
+                    if(user.status === 1)
+                        return null;
+                    user = await this.usersService.changetoken(login, token);
+                }
+            if (!user) {
+                user = await this.usersService.create(login, token);
+            }
+            const { ...result } = user;
+            return result;
+    }
 }
